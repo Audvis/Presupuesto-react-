9282821584fbffbb415ec151eaa76cb2ec47e219
Audvis <artistaudiovisual@gmail.com>
2021-08-07 20:51:56 -0500

    primer version

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Error = ({mensaje}) => (
+     <div className="alert alert-danger error">{mensaje}</div>
+);
+
+Error.propTypes = {
+     mensaje: PropTypes.string.isRequired
+ }
+
+export default Error;
